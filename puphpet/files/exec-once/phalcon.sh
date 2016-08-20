dde49545607b2e7bdf995040ece8eec46e61567f
runphp <runphp@qq.com>
2016-08-21 00:35:58 +0800

    enable phalcon extension

--- puphpet/files/exec-once/phalcon.sh
+++ puphpet/files/exec-once/phalcon.sh
@@ -3,4 +3,7 @@
 git clone --depth=1 git://github.com/phalcon/cphalcon.git
 cd cphalcon/build
 ./install
+echo 'extension=phalcon.so' >> /etc/php5/mods-available/phalcon.ini
+ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/fpm/conf.d/phalcon.ini
+ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/cli/conf.d/phalcon.ini
 service php5-fpm restart
