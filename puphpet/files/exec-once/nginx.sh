20e317bd8b773e63500868e1c089e5b0ecc4e971
runphp <runphp@qq.com>
2016-08-24 20:50:00 +0800

    fix duplicate phalcon module

--- puphpet/files/exec-once/nginx.sh
+++ puphpet/files/exec-once/nginx.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+# /puphpet/files/exec-once/nginx.sh
+
 sudo sed -i '11a include /var/www/www.eelly.dev/rewrite.nginx;' /etc/nginx/sites-available/www_eelly_dev.conf
-sudo sed -i '11a include /var/www/so.eelly.dev/rewrite.nginx;' /etc/nginx/sites-available/so_eelly_dev.conf 
+sudo sed -i '11a include /var/www/so.eelly.dev/rewrite.nginx;' /etc/nginx/sites-available/so_eelly_dev.conf
+sudo service nginx restart
