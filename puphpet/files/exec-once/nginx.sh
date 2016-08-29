da56a748e8df3c274eaf1dbee3da4d5050d379dc
runphp <runphp@qq.com>
2016-08-29 15:09:31 +0800

    add php mongo

--- puphpet/files/exec-once/nginx.sh
+++ puphpet/files/exec-once/nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # /puphpet/files/exec-once/nginx.sh
 
-sudo sed -i '11a include /var/www/www.eelly.dev/rewrite.nginx;' /etc/nginx/sites-available/www_eelly_dev.conf
-sudo sed -i '11a include /var/www/so.eelly.dev/rewrite.nginx;' /etc/nginx/sites-available/so_eelly_dev.conf
-sudo service nginx restart
+sed -i '11a include /var/www/www.eelly.dev/rewrite.nginx;' /etc/nginx/sites-available/www_eelly_dev.conf
+sed -i '11a include /var/www/so.eelly.dev/rewrite.nginx;' /etc/nginx/sites-available/so_eelly_dev.conf
+service nginx restart
